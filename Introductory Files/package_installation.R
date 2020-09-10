9693a1ebf1d9b960bee2492fde62b8b71ab4a286
AVS <36423949+AShrivats@users.noreply.github.com>
2020-09-10 16:53:51 -0400

    Re-organizing
    praying this doesn't screw anything up, don't really know what I'm doing.

--- Introductory Files/package_installation.R
+++ Introductory Files/package_installation.R
@@ -7,6 +7,8 @@ install.packages("ggforce") # an extension to ggplot - used in circles example
 install.packages("devtools") # this package allows us to install packages from github links
 devtools::install_github("cutterkom/generativeart") # generativeart package also used in some examples
 
+install.packages("colourlovers") # gives us nice and pretty colour pallettes to use
+
 # subsumes ggplot2, also adds a bunch of other packages that the examples take advantage of
 install.packages("tidyverse") 
 
@@ -14,6 +16,7 @@ library(ggplot2)
 library(ggforce)
 library(generativeart)
 library(tidyverse)
+library(colourlovers)
 
 # other useful items
 # list of R colors: http://www.stat.columbia.edu/~tzheng/files/Rcolor.pdf
