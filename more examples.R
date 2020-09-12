7bb43b34047c9631f155174a2ab7922654466a45
Blair Bilodeau <blair.bilodeau@mail.utoronto.ca>
2020-09-12 12:35:22 -0400

    started adding comments to more examples

--- more examples.R
+++ more examples.R
@@ -1,17 +1,39 @@
-#More small examples
-# vertiginous spiral
+# More small examples
+library(ggplot2)
 library(tidyverse)
-df <- data.frame(x=0, y=0)
-for (i in 2:500){
-  df[i,1] <- df[i-1,1]+((0.98)^i)*cos(i)
-  df[i,2] <- df[i-1,2]+((0.98)^i)*sin(i)   
+
+# vertiginous spiral
+numcoords <- 10 # number of coordinates
+numvert <- 8 # number of vertices
+
+xmult <- 30
+ymult <- 14
+
+ids <- as.factor(1:numcoords) 
+values <- data.frame(
+  id = ids,
+  value = seq(1,2,1/(numcoords-1))
+) 
+
+xvals <- numeric(numcoords*numvert)
+yvals <- numeric(numcoords*numvert)
+for (i in 2:(numcoords*numvert)){
+  xvals[i] <- xvals[i-1] + ((xmult)^i)*cos(i)
+  yvals[i] <- yvals[i-1] + ((ymult)^i)*sin(i)   
 }
-ggplot(df, aes(x,y)) + 
-  geom_polygon()+
-  theme_void()
+
+coords <- data.frame(
+  id = rep(ids, each = numvert),
+  x = xvals,
+  y = yvals
+)
+
+datapoly <- merge(values, coords, by = c("id"))
+
+ggplot(datapoly, aes(x = x, y = y)) +
+  geom_polygon(aes(fill = value, group = id))
 
 # marine creature
-library(tidyverse)
 seq(from=-10, to=10, by = 0.05) %>%
   expand.grid(x=., y=.) %>%
   ggplot(aes(x=(x^2+pi*cos(y)^2), y=(y+pi*sin(x)))) +
@@ -19,7 +41,6 @@ seq(from=-10, to=10, by = 0.05) %>%
   theme_void()+coord_fixed()
 
 # summoning cthultu
-library(tidyverse)
 seq(-3,3,by=.01) %>%
   expand.grid(x=., y=.) %>%
   ggplot(aes(x=(x^3-sin(y^2)), y=(y^3-cos(x^2)))) +
@@ -29,8 +50,7 @@ seq(-3,3,by=.01) %>%
   theme(panel.background = element_rect(fill="black"))+
   coord_polar()
 
-#naive sunflower
-library(ggplot2)
+# naive sunflower
 a=pi*(3-sqrt(5))
 n=500
 ggplot(data.frame(r=sqrt(1:n),t=(1:n)*a),
