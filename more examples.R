b5645aa1e60435daa5d03dc5fe733c9b17458602
Blair Bilodeau <blair.bilodeau@mail.utoronto.ca>
2020-09-12 12:38:52 -0400

    ..

--- more examples.R
+++ more examples.R
@@ -1,37 +1,15 @@
-# More small examples
-library(ggplot2)
-library(tidyverse)
-
-# vertiginous spiral
-numcoords <- 10 # number of coordinates
-numvert <- 8 # number of vertices
-
-xmult <- 30
-ymult <- 14
-
-ids <- as.factor(1:numcoords) 
-values <- data.frame(
-  id = ids,
-  value = seq(1,2,1/(numcoords-1))
-) 
-
-xvals <- numeric(numcoords*numvert)
-yvals <- numeric(numcoords*numvert)
-for (i in 2:(numcoords*numvert)){
-  xvals[i] <- xvals[i-1] + ((xmult)^i)*cos(i)
-  yvals[i] <- yvals[i-1] + ((ymult)^i)*sin(i)   
-}
-
-coords <- data.frame(
-  id = rep(ids, each = numvert),
-  x = xvals,
-  y = yvals
-)
-
-datapoly <- merge(values, coords, by = c("id"))
-
-ggplot(datapoly, aes(x = x, y = y)) +
-  geom_polygon(aes(fill = value, group = id))
+#More small examples
+# need tidyverse for all of these
+# vertiginous spiral doesn't seem to work - ask blair to check if its just me
+# # vertiginous spiral
+# df <- data.frame(x=0, y=0)
+# for (i in 2:500){
+#   df[i,1] <- df[i-1,1]+((0.98)^i)*cos(i)
+#   df[i,2] <- df[i-1,2]+((0.98)^i)*sin(i)   
+# }
+# ggplot(df, aes(x,y)) + 
+#   geom_polygon()+
+#   theme_void()
 
 # marine creature
 seq(from=-10, to=10, by = 0.05) %>%
@@ -50,7 +28,7 @@ seq(-3,3,by=.01) %>%
   theme(panel.background = element_rect(fill="black"))+
   coord_polar()
 
-# naive sunflower
+#naive sunflower - needs ggplot2
 a=pi*(3-sqrt(5))
 n=500
 ggplot(data.frame(r=sqrt(1:n),t=(1:n)*a),
